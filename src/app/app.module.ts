d3854875bffa71a1e2d2c1369af89fdb23983e47
Joaquin Philip Tandang <joaquinphilip.tandang@evsu.edu.ph>
2021-10-28 18:02:51 +0800

    A lot of edits.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "**", redirectTo: "home"}
+]
 
 @NgModule({
   declarations: [
@@ -12,7 +18,8 @@ import { HomeComponent } from './pages/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot (routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
